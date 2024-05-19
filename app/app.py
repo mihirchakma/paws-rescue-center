bb54822388155abafda11adb35a9d45c6f04ad07
Mihir Chakma <mihirchakmaths@gmail.com>
2024-05-19 22:34:55 +0530

    add logout and validations

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 """Flask Application for Paws Rescue Center."""
 from flask import Flask, render_template, abort
 from forms import SignUpForm, LoginForm
+from flask import session, redirect, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
@@ -15,8 +16,9 @@ pets = [
 
 """Information regarding the Users in the System."""
 users = [
-    {"id": 1, "full_name":"Pet Rescue Team", "email":"team@pawsrescue.com", "password":"adminpass"}
+    {"id": 1, "full_name":"Pet Rescue Team", "email":"team@pawsrescue.com", "password":"adminpass"},
 ]
+
 
 @app.route("/")
 def homepage():
@@ -38,6 +40,7 @@ def pet_details(pet_id):
         abort(404, description="No Pet was Found with the given ID")
     return render_template("details.html", pet = pet)
 
+
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     """View function for Showing Details of Each Pet."""
@@ -48,10 +51,25 @@ def signup():
         return render_template("signup.html", message = "Successfully signed up")    
     return render_template("signup.html", form = form)
 
+
 @app.route("/login", methods = ["POST", "GET"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = next((user for user in users if user["email"] == form.email.data and user["password"] == form.password.data), None)
+        if user is None:
+            return render_template("login.html", form = form, message = "Wrong Credentials. Please Try Again.")
+        else:
+            session['user'] = user
+            return render_template("login.html", message = "Successfully Log In!")
     return render_template("login.html", form = form)
+
+
+@app.route("/logout")
+def logout():
+    if 'user' in session:
+        session.pop('user')
+    return redirect(url_for('homepage', _scheme = 'https', _external = True))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=3000)
